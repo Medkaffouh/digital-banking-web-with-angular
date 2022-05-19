4c87aa456ee947017bcf4a8db48167b6d83993db
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-19 14:36:22 +0100

    implementé les 3 cas success loading error

--- src/app/customers/customers.component.ts
+++ src/app/customers/customers.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {CustomerService} from "../services/customer.service";
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Customer } from '../model/customer.model';
 
 @Component({
   selector: 'app-customers',
@@ -9,12 +10,17 @@ import { Observable } from 'rxjs';
   styleUrls: ['./customers.component.css']
 })
 export class CustomersComponent implements OnInit {
-  customers! : Observable<any>;
+  customers! : Observable<Array<Customer>>;
   errorMessage! : string; // OU errorMessage : string | undefined
   constructor(private customerService:CustomerService) { }
 
   ngOnInit(): void {
-    this.customers = this.customerService.getCustomers();
+    this.customers = this.customerService.getCustomers().pipe(
+      catchError(err=>{
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
   }
 
 }
