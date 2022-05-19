6c22fbc2dda58cd658ca89e0b9ae16b33e252c68
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-19 13:14:29 +0100

    creation d'une customer model

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import { Customer } from '../model/customer.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class CustomerService {
 
   constructor(private http:HttpClient) { }
 
-  public getCustomers():Observable<any>{
-    return this.http.get("http://localhost:8082/customers")
+  public getCustomers():Observable<Array<Customer>>{
+    return this.http.get<Array<Customer>>("http://localhost:8082/customers")
   }
 }
